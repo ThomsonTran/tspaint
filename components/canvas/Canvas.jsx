fd56db107e782c6dd8ed0c325d89aea398e54f8c
Thomson <thomson@livescale.tv>
2024-04-21 21:52:12 -0400

    simplified the useEffects(), expanded out props

--- components/canvas/Canvas.jsx
+++ components/canvas/Canvas.jsx
@@ -8,97 +8,50 @@ import React, {
 import SettingsContext from "../SettingsContext";
 import Tools from "./Tools";
 
-const Canvas = (props) => {
+const Canvas = ({ width, height }) => {
     const [isPainting, setIsPainting] = useState(false);
     const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-
     const canvasRef = useRef(null);
     const settings = useContext(SettingsContext);
-
-    const width = props.width;
-    const height = props.height;
-
     const { gridSize } = useContext(SettingsContext);
 
     useEffect(() => {
         if (!canvasRef.current) {
             return;
         }
-        const getCoordinates = (event) => {
-            const canvas = canvasRef.current;
-            return {
-                x: event.pageX - canvas.offsetLeft,
-                y: event.pageY - canvas.offsetTop,
-            };
-        };
-
-        const startPaint = (event) => {
-            const coordinates = getCoordinates(event);
-            if (coordinates) {
-                setMousePosition(coordinates);
-                setIsPainting(true);
-            }
-        };
 
         const canvas = canvasRef.current;
-        canvas.addEventListener("mousedown", startPaint);
-        return () => {
-            canvas.removeEventListener("mousedown", startPaint);
-        };
+        const context = canvas.getContext("2d");
+        context.fillStyle = "#FFFFFF";
+        context.fillRect(0, 0, width, height);
+        context.beginPath();
+        context.lineWidth = "1";
+        context.strokeStyle = "blue";
+        for (let y = 0; y < height; y += gridSize) {
+            context.moveTo(0, y);
+            context.lineTo(width, y);
+        }
+        for (let x = 0; x < width; x += gridSize) {
+            context.moveTo(x, 0);
+            context.lineTo(x, height);
+        }
+        context.stroke();
+    }, [width, height, gridSize]);
+
+    const startPaint = useCallback((event) => {
+        const coordinates = getCoordinates(event);
+        if (coordinates) {
+            setMousePosition(coordinates);
+            setIsPainting(true);
+        }
     }, []);
 
     const paint = useCallback(() => {
-        if (!canvasRef.current) {
-            return;
-        }
-
-        if (isPainting) {
-            const canvas = canvasRef.current;
-            const context = canvas.getContext("2d");
-
-            Tools[settings.tool].draw(mousePosition, context, settings);
-        }
-    }, [isPainting, mousePosition, settings]);
-
-    useEffect(() => {
-        if (!canvasRef.current) {
-            return;
-        }
+        if (!canvasRef.current || !isPainting) return;
         const canvas = canvasRef.current;
-        canvas.addEventListener("click", paint);
-        return () => {
-            canvas.removeEventListener("click", paint);
-        };
-    }, [paint]);
-
-    useEffect(() => {
-        if (!canvasRef.current) {
-            return;
-        }
-        const createGrid = (gridSize) => {
-            gridSize = Math.max(gridSize, 1);
-            const canvas = canvasRef.current;
-            const context = canvas.getContext("2d");
-
-            context.fillStyle = "#FFFFFF";
-            context.fillRect(0, 0, width, height);
-            context.beginPath();
-            context.lineWidth = "1";
-            context.strokeStyle = "blue";
-
-            for (let y = 0; y < height; y += gridSize) {
-                context.moveTo(0, y);
-                context.lineTo(width, y);
-            }
-            for (let x = 0; x < width; x += gridSize) {
-                context.moveTo(x, 0);
-                context.lineTo(x, height);
-            }
-            context.stroke();
-        };
-
-        createGrid(gridSize);
-    }, [height, width, gridSize]);
+        const context = canvas.getContext("2d");
+        Tools[settings.tool].draw(mousePosition, context, settings);
+    }, [isPainting, mousePosition, settings]);
 
     const exitPaint = useCallback(() => {
         setIsPainting(false);
@@ -110,18 +63,30 @@ const Canvas = (props) => {
             return;
         }
         const canvas = canvasRef.current;
+        canvas.addEventListener("mousedown", startPaint);
+        canvas.addEventListener("click", paint);
         canvas.addEventListener("mouseleave", exitPaint);
         return () => {
+            canvas.removeEventListener("mousedown", startPaint);
+            canvas.removeEventListener("click", paint);
             canvas.removeEventListener("mouseleave", exitPaint);
         };
-    }, [exitPaint]);
+    }, [startPaint, paint, exitPaint]);
+
+    const getCoordinates = (event) => {
+        const canvas = canvasRef.current;
+        return {
+            x: event.pageX - canvas.offsetLeft,
+            y: event.pageY - canvas.offsetTop,
+        };
+    };
 
     return (
         <canvas
-            className="canvasBackground"
+            className="m-auto border border-blue-500"
             ref={canvasRef}
-            height={height}
             width={width}
+            height={height}
         />
     );
 };
