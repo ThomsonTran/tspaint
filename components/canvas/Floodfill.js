51a19789772a72a7be19990a11d119b4592dd856
Thomson <thomson@livescale.tv>
2024-04-21 23:31:30 -0400

    changed unneeded .jsx files to .js

--- components/canvas/Floodfill.js
+++ components/canvas/Floodfill.js
@@ -19,7 +19,7 @@ function getHex(pixelColor) {
 
 function rgbToHex(r, g, b) {
     if (r > 255 || g > 255 || b > 255) {
-        return console.error("invalid rgb");
+        return "#000000";
     }
     return ((r << 16) | (g << 8) | b).toString(16);
 }
